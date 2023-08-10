2438ebf8dd352102b563632610e4effdeb1b3b83
Freddy Matute <freddy.matute@outlook.com>
2023-08-09 21:01:35 -0500

    Agregué la función seno

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ def calcula_cos(value):
     return math.cos(value)
 
 print ("Coseno_x{}".format(calcula_cos(20)))
+
+def calcula_sen(value):
+    return math.sin(value)
+
+print ("Seno_x{}".format(calcula_sen(20)))
